a1d9861651c2bf724c24dc2c241ea8bda8781e6a
Esteban Morales <jackietoyt@gmail.com>
2025-04-14 13:54:40 +0200

    starting exercise 2 of day 2

---
+++ 02_Day_Data_types/02_day_starter/exercise2.js
@@ -0,0 +1,9 @@
+console.log("EXERCISE 2")
+
+//1
+quote = "'There is no exercise better for the heart than reaching down and lifting people up'"
+console.log('The quote ' + quote + 'by John Holmes teaches us to help one another')
+
+//2
+quote2 = "\"Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.\""
+console.log(quote2)
